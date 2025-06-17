dbc269772c8312c937bff121d0ebbcbebfcd5cb7
Carlos P <2123110286@untels.edu.pe>
2025-06-16 22:35:50 -0500

    cambiando al nuevo 0.1

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -5,7 +5,7 @@ class Conexion
     public function conectarbd()
     {
         try {
-            $this->conn = new mysqli("localhost", "root", "Jorge.02.Bravo.09", "emfermedades_db");
+            $this->conn = new mysqli("localhost", "root", "", "enfermedades_db");
             // Verificar si la conexión fue exitosa
             if ($this->conn->connect_error) {
                 die("Error de conexión: " . $this->conn->connect_error);
